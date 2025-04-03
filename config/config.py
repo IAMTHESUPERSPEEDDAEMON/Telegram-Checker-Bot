394d0e34b1add8b78317c3990eaa8f87b93151a8
OSwego <syava23223@gmail.com>
2025-04-03 14:44:36 +0300

    add .env file

--- config/config.py
+++ config/config.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 # Настройки базы данных
 DB_CONFIG = {
-    'host': os.getenv('DB_HOST', 'localhost'),
-    'user': os.getenv('DB_USER', 'root'),
-    'password': os.getenv('DB_PASSWORD', '390492ufhjOy*&rt^&FR723'),
-    'database': os.getenv('DB_NAME', 'telegram_checker'),
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'database': os.getenv('DB_NAME'),
     'port': int(os.getenv('DB_PORT', 3306))
 }
 
